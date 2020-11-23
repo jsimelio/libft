297ebd8319922619dc77dee85ecdbb8682fd8c73
jsimeli0 <69462144+jsimeli0@users.noreply.github.com>
2020-11-23 17:02:24 +0100

    deleted trash files

--- README.md
+++
@@ -1,4 +0,0 @@
-# libft
-Repo for the Codam libft project
-
-Jorge here. First attempt at using github to organize my Codam work.
--- tempCodeRunnerFile.c
+++
@@ -1,4 +0,0 @@
-	// memset(dest, 'r', 14);
-	// ft_print_result(ft_strlcat(dest, "lorem ipsum dolor sit amet", 15));
-	// write(1, "\n", 1);
-	// write(1, dest, 15);
